b2726c8875d72f77469987ce8e87c701015c3401
madhuripatil06 <madhuri@thoughtworks.com>
2016-10-28 14:56:03 +0530

    draw simple divs for 10 numbers

---
+++ public/javascript/animationByDiv.js
@@ -0,0 +1,15 @@
+var draw = function(){
+	var container = d3.select(".container");
+	var data = [1,2,3,4,5,6,7,8,9,10];
+
+	container.selectAll("div").data(data)
+		.enter()
+		.append("div")
+		.style("width", function(d){ return d*50})
+		.style("height",30)
+		.style("background-color", "blue");
+
+}
+
+
+draw();
