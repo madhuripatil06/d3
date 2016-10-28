2e66c1083e2468d8a63f0b888ca5856843d6ffac
madhuripatil06 <madhuri@thoughtworks.com>
2016-10-28 15:08:31 +0530

    random numbers bar chart

--- public/javascript/animationByDiv.js
+++ public/javascript/animationByDiv.js
@@ -1,12 +1,21 @@
+var randomNumbers = function(count){
+	var result = [];
+	for(var i = 0 ; i < count ; i++){
+		result.push(Math.ceil(Math.random()*100));
+	};
+	return result;
+}
+
 var draw = function(){
 	var container = d3.select(".container");
-	var data = [1,2,3,4,5,6,7,8,9,10];
+	var data = randomNumbers(10);
 
 	container.selectAll("div").data(data)
 		.enter()
 		.append("div")
-		.style("width", function(d){ return d*50})
+		.style("width", function(d){ return d*10})
 		.classed("bar", true)
+		.text(function(d){ return d});
 }
 
 
