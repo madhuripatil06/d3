91bba0eec8919a6223e50783281c1eb6227981b3
madhuripatil06 <madhuri@thoughtworks.com>
2016-11-11 11:56:18 +0530

    refactor

--- public/javascript/table.js
+++ public/javascript/table.js
@@ -14,28 +14,27 @@ var drawHeaders = function(){
 	tr.enter()
 		.append("td")
 		.html(function(d){ return d});
-}
+};
 
 var drawNumbers = function(){
 	appendNewRow("value").enter()
 		.append("td")
 		.html(function(d){ return d});
-}
+};
 
 var drawSquares = function(){
 	var square = d3.scalePow().exponent(2);
 	appendNewRow("square").enter()
 		.append("td")
 		.html(function(d){ return square(d)});
-}
+};
 
 var drawLog = function(){
 	var logScale = d3.scaleLog();
 	appendNewRow("log(n)").enter()
 		.append("td")
 		.html(function(d){ return d3.format(".5f")(logScale(d))});
-}
-
+};
 
 var drawRoundLog = function(){
 	var log = d3.scaleLog();
@@ -43,7 +42,7 @@ var drawRoundLog = function(){
 	appendNewRow("round(log(n))").enter()
 		.append("td")
 		.html(function(d){ return round(log(d))});
-}
+};
 
 var load = function(){
 	var table = d3.select("body").append("table")
