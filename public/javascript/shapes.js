2a818a83610a09d12a74cbf4520fdf99b92cd950
madhuripatil06 <madhuri@thoughtworks.com>
2016-11-15 15:45:12 +0530

    refactor

--- public/javascript/shapes.js
+++ public/javascript/shapes.js
@@ -1,6 +1,6 @@
 const SHAPE_SIZE = 100; 
 const SVG_WIDTH = 600; 
-const SVG_HEIGHT = 100;
+const SVG_HEIGHT = SHAPE_SIZE;
 const MARGIN = 50; 
 
 
@@ -8,47 +8,61 @@ var translate = function(x,y){
 	return "translate(" + x + ", " + y + ")";
 }
 
-var drawLine = function(svg, i){
+var drawLine = function(svg){
 	svg.append("line")
 		.classed("shape", true)
-		.attr("x1", 100)
+		.attr("x1", SHAPE_SIZE)
 		.attr("y1", 0)
 		.attr("x2", 0)
 		.attr("y2", SHAPE_SIZE)
+		.attr("stroke", "black");
 }
 
-var drawRect = function(svg, i){
+var drawRect = function(svg){
 	svg.append("rect")
 		.attr("width", SHAPE_SIZE)
 		.attr("height", SHAPE_SIZE)
 		.attr("rx", 10)
 		.attr("ry", 10)
-		.classed("shape", true)
-		.attr("transform", translate((i*50)+50,0));
+		.attr("stroke", "blue")
+		.classed("shape", true);
 }
 
-var drawCircle = function(svg, i){
+var drawCircle = function(svg){
 	svg.append("circle")
-		.attr("r", 50)
-		.classed("shape", true)
-		.attr("transform", translate((i*100)+50,MARGIN));
+		.attr("r", SHAPE_SIZE/2)
+		.attr("cx", SHAPE_SIZE/2)
+		.attr("cy", SHAPE_SIZE/2)
+		.attr("stroke", "red")
+		.classed("shape", true);
 }
 
-var drawPolygon = function(svg, i){
-	var points = MARGIN+",0 0,"+SHAPE_SIZE+" "+SHAPE_SIZE+","+SHAPE_SIZE;
+var drawTriangle = function(svg){
+	var points = SHAPE_SIZE/2+",0 0,"+SHAPE_SIZE+" "+SHAPE_SIZE+","+SHAPE_SIZE;
 	svg.append("polygon")
 		.attr("points", points)
-		.classed("shape", true)
-		.attr("transform", translate((i*100)+50,0));
+		.attr("stroke", "green")
+		.classed("shape", true);
 }
+
+var transition = function(){
+	d3.selectAll(".shape")
+		.transition()
+		.duration(2000)
+		.attr("transform", function(d, i){ return translate((i*(SHAPE_SIZE+MARGIN)), 0)});
+};
+var operations = [drawLine, drawRect, drawCircle, drawTriangle];
+
 var load = function(){
-	var operations = [drawLine, drawRect, drawCircle, drawPolygon];
 	var svg = d3.select(".container").append("svg")
 		.attr("height", SVG_HEIGHT)
-		.attr("width", SVG_WIDTH)
+		.attr("width", SVG_WIDTH);
+
 	for (var i = 0; i < operations.length; i++) {
-		operations[i](svg, i+1);
+		operations[i](svg);
 	};
+	transition();
+	
 }
 
 window.onload = load;
