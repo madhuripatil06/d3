121b92fbbae53f7a26543e41222fec0218759a68
madhuripatil06 <madhuri@thoughtworks.com>
2016-11-15 14:36:55 +0530

    remove hard code from transform translate

--- public/javascript/shapes.js
+++ public/javascript/shapes.js
@@ -3,51 +3,52 @@ const SVG_WIDTH = 600;
 const SVG_HEIGHT = 100;
 const MARGIN = 50; 
 
+
 var translate = function(x,y){
 	return "translate(" + x + ", " + y + ")";
 }
 
-var drawLine = function(svg){
+var drawLine = function(svg, i){
 	svg.append("line")
 		.classed("shape", true)
-		.attr("x1", MARGIN)
+		.attr("x1", 100)
 		.attr("y1", 0)
 		.attr("x2", 0)
 		.attr("y2", SHAPE_SIZE)
 }
 
-var drawRect = function(svg){
+var drawRect = function(svg, i){
 	svg.append("rect")
 		.attr("width", SHAPE_SIZE)
 		.attr("height", SHAPE_SIZE)
 		.attr("rx", 10)
 		.attr("ry", 10)
 		.classed("shape", true)
-		.attr("transform", translate(100,0));
+		.attr("transform", translate((i*50)+50,0));
 }
 
-var drawCircle = function(svg){
+var drawCircle = function(svg, i){
 	svg.append("circle")
 		.attr("r", 50)
 		.classed("shape", true)
-		.attr("transform", translate(300,MARGIN));
+		.attr("transform", translate((i*100)+50,MARGIN));
 }
 
-var drawPolygon = function(svg){
+var drawPolygon = function(svg, i){
 	var points = MARGIN+",0 0,"+SHAPE_SIZE+" "+SHAPE_SIZE+","+SHAPE_SIZE;
 	svg.append("polygon")
 		.attr("points", points)
 		.classed("shape", true)
-		.attr("transform", translate(400,0));
+		.attr("transform", translate((i*100)+50,0));
 }
 var load = function(){
+	var operations = [drawLine, drawRect, drawCircle, drawPolygon];
 	var svg = d3.select(".container").append("svg")
 		.attr("height", SVG_HEIGHT)
 		.attr("width", SVG_WIDTH)
-	drawLine(svg);
-	drawCircle(svg);
-	drawRect(svg);
-	drawPolygon(svg);
+	for (var i = 0; i < operations.length; i++) {
+		operations[i](svg, i+1);
+	};
 }
 
 window.onload = load;
